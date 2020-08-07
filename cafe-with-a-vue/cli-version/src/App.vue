8338b8ee5a0fba9fe0690000e20f498cb2d6e124
Tanneguy Leveque <tanleveque@gmail.com>
2020-08-07 15:50:48 +0200

    P2C2 Begin try 1

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,24 +1,114 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+      notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+      matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+      est difficile de s'arrêter.
+    </p>
+
+    <section class="menu">
+      <h2>Menu</h2>
+      <MenuItem name image inStock quantity></MenuItem>
+    </section>
+
+    <aside class="shopping-cart">
+      <h2>Panier d'achat : {{ shoppingCart }} articles</h2>
+    </aside>
+
+    <h2>Contactez nous</h2>
+    <p>Adresse : {{ address }}</p>
+    <p>Téléphone : {{ phone }}</p>
+    <p>Email : {{ email }}</p>
+    <p>Horaires :</p>
+    <ul>
+      <li>L-V: 06:00 à 16:00</li>
+      <li>Samedi: 07:00 à 14:00</li>
+      <li>Dimanche: 07:00 à 12:00</li>
+    </ul>
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import MenuItem from "./components/MenuItem.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
+
+export default {
+  name: "app",
+  components: {
+    MenuItem,
+  },
+};
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    address: "18 avenue du Beurre, Paris, France",
+    email: "hello@cafewithavue.bakery",
+    phone: "01 88 88 88 88",
+    restaurantName: "La belle vue",
+    shoppingCart: 0,
+    simpleMenu: [
+      {
+        name: "Croissant",
+        image: {
+          source: "/images/crossiant.jpg",
+          alt: "Un croissant",
+        },
+        inStock: true,
+        quantity: 1,
+      },
+      {
+        name: "Baguette de pain",
+        image: {
+          source: "/images/french-baguette.jpeg",
+          alt: "Quatre baguettes de pain",
+        },
+        inStock: true,
+        quantity: 1,
+      },
+      {
+        name: "Éclair",
+        image: {
+          source: "/images/eclair.jpg",
+          alt: "Éclair au chocolat",
+        },
+        inStock: false,
+        quantity: 1,
+      },
+    ],
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear();
+
+      return `Copyright ${this.restaurantName} ${currentYear}`;
+    },
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount;
+    },
+  },
+});
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="item in simpleMenu" :key="item.name" class="menu-item">
+      <img class="menu-item__image" :src="item.image.source" :alt="item.image.alt" />
+      <div>
+        <h3>{{ item.name }}</h3>
+        <p v-if="item.inStock">En stock</p>
+        <p v-else>En rupture de stock</p>
+        <div>
+          <label for="add-item-quantity">Quantité : {{ item.quantity }}</label>
+          <input v-model.number="item.quantity" id="add-item-quantity" type="number" />
+          <button @click="addToShoppingCart(item.quantity)">Ajouter au panier d'achat</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+// ./components/MenuItem.vue
+<script>
+export default {
+  name: "MenuItem",
+  props: ["name", "image", "inStock", "quantity"],
+};
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
