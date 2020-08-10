29256b91a586b072ffafc430fff4e6777a5fd8bb
Tanneguy Leveque <tanleveque@gmail.com>
2020-08-10 14:52:30 +0200

    P2C4 begin with solu

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,27 +1,46 @@
+<template>
+  <div class="menu-item">
+    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
+    <div>
+      <h3>{{ name }}</h3>
+      <p>Prix : {{ calcPrice }} </p>
+      <p v-if="inStock">En stock</p>
+      <p v-else>En rupture de stock</p>
+      <div>
+        <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+        <input v-model.number="quantity" id="add-item-quantity" type="number" />
+        <button @click="addToShoppingCart(quantity)">Ajouter au panier</button>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
-	name: "MenuItem",
-	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"]
-}
-</script>
+  name: "MenuItem",
+  props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+  data() {
+    return {
+      onSale: false,
+    };
+  },
+  computed: {
+    calcPrice() {
+		if (this.onSale ){
+			return (this.price * 0.9).toFixed(2)
+		} else { 
+			return this.price
+		}
+    },
+  },
+  beforeMount() {
+    const today = new Date().getDate();
 
-<template>
-	<div class="menu-item">
-		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
-		<div>
-			<h3>{{ name }}</h3>
-			<p>Prix : {{ price }}</p>
-			<p v-if="inStock">En stock</p>
-			<p v-else>En rupture de stock</p>
-			<div>
-				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
-				<input v-model.number="quantity" id="add-item-quantity" type="number" />
-				<button @click="addToShoppingCart(quantity)">
-					Ajouter au panier
-				</button>
-			</div>
-		</div>
-	</div>
-</template>
+    if (today % 2 === 0) {
+      this.onSale = true;
+    }
+  },
+};
+</script>
 
 <style></style>
--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -1,94 +1,94 @@
 <template>
-	<div id="app" class="app">
-		<h1>{{ restaurantName }}</h1>
-		<p class="description">
-			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
-			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
-			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
-			est difficile de s'arrêter.
-		</p>
+  <div id="app" class="app">
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+      notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+      matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+      est difficile de s'arrêter.
+    </p>
 
-		<section class="menu">
-			<h2>Menu</h2>
-			<MenuItem
-				v-for="item in simpleMenu"
-				:addToShoppingCart="addToShoppingCart"
-				:name="item.name"
-				:image="item.image"
-				:price="item.price"
-				:quantity="item.quantity"
-				:inStock="item.inStock"
-				:key="item.name"
-			/>
-		</section>
+    <section class="menu">
+      <h2>Menu</h2>
+      <MenuItem
+        v-for="item in simpleMenu"
+        :addToShoppingCart="addToShoppingCart"
+        :name="item.name"
+        :image="item.image"
+        :price="item.price"
+        :quantity="item.quantity"
+        :inStock="item.inStock"
+        :key="item.name"
+      />
+    </section>
 
-		<aside class="shopping-cart">
-			<h2>Panier : {{ shoppingCart }} articles</h2>
-		</aside>
+    <aside class="shopping-cart">
+      <h2>Panier : {{ shoppingCart }} articles</h2>
+    </aside>
 
-		<footer class="footer">
-			<p>{{ copyright }}</p>
-		</footer>
-	</div>
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
+  </div>
 </template>
 
 <script>
-import MenuItem from "../components/MenuItem"
+import MenuItem from "../components/MenuItem";
 
 export default {
-	name: "Home",
-	components: {
-		MenuItem
-	},
-	data() {
-		return {
-			restaurantName: "La belle vue",
-			shoppingCart: 0,
-			simpleMenu: [
-				{
-					name: "Croissant",
-					image: {
-						source: "/images/crossiant.jpg",
-						alt: "Un croissant"
-					},
-					inStock: true,
-					quantity: 1,
-					price: 2.99
-				},
-				{
-					name: "Baguette de pain",
-					image: {
-						source: "/images/french-baguette.jpeg",
-						alt: "Quatre baguettes de pain"
-					},
-					inStock: true,
-					quantity: 1,
-					price: 3.99
-				},
-				{
-					name: "Éclair",
-					image: {
-						source: "/images/eclair.jpg",
-						alt: "Éclair au chocolat"
-					},
-					inStock: false,
-					quantity: 1,
-					price: 4.99
-				}
-			]
-		}
-	},
-	computed: {
-		copyright() {
-			const currentYear = new Date().getFullYear()
+  name: "Home",
+  components: {
+    MenuItem,
+  },
+  data() {
+    return {
+      restaurantName: "La belle vue",
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: "Croissant",
+          image: {
+            source: "/images/crossiant.jpg",
+            alt: "Un croissant",
+          },
+          inStock: true,
+          quantity: 1,
+          price: 2.99,
+        },
+        {
+          name: "Baguette de pain",
+          image: {
+            source: "/images/french-baguette.jpeg",
+            alt: "Quatre baguettes de pain",
+          },
+          inStock: true,
+          quantity: 1,
+          price: 3.99,
+        },
+        {
+          name: "Éclair",
+          image: {
+            source: "/images/eclair.jpg",
+            alt: "Éclair au chocolat",
+          },
+          inStock: false,
+          quantity: 1,
+          price: 4.99,
+        },
+      ],
+    };
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear();
 
-			return `Copyright ${this.restaurantName} ${currentYear}`
-		}
-	},
-	methods: {
-		addToShoppingCart(amount) {
-			this.shoppingCart += amount
-		}
-	}
-}
+      return `Copyright ${this.restaurantName} ${currentYear}`;
+    },
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount;
+    },
+  },
+};
 </script>
